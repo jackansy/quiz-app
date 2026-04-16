0beb56b11ba280f11fe191c5238e106c2fb65f09
Aditya Gupta <adityagupta4884@gmail.com>
2026-04-16 13:02:52 +0530

    fixed questions export

---
+++ app/data/questions.js
@@ -0,0 +1,32 @@
+export const questions = {
+  week1: [
+    {
+      question: "Which of the following is an Indian-origin diversified business group?",
+      options: ["Unilever", "Tata Group", "Amazon", "Microsoft"],
+      answer: "Tata Group",
+    },
+    {
+      question: "Which represents social environment?",
+      options: [
+        "Social values and traditions",
+        "Interest rates",
+        "Production",
+        "Technology",
+      ],
+      answer: "Social values and traditions",
+    },
+  ],
+
+  week2: [
+    {
+      question: "What defines a company's vision?",
+      options: [
+        "Short-term goal",
+        "Long-term aspiration",
+        "Salary plan",
+        "Daily tasks",
+      ],
+      answer: "Long-term aspiration",
+    },
+  ],
+};
